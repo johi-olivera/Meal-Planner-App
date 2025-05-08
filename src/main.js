86eedf91ef9b7d984d457eabd5cc73c793a0b5d9
Johanna Olivera <johiolivera@gmail.com>
2025-05-07 21:46:22 -0300

    Set up Electron project with initial structure

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'renderer.js') // src/renderer.js
+    }
+  });
+
+  // Cargar la vista principal desde views/pages
+  win.loadFile(path.join(__dirname, '../views/pages/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ src/renderer.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const addMealBtn = document.getElementById('addMealBtn');
+    addMealBtn.addEventListener('click', () => {
+      alert('Meal added! (Placeholder)');
+    });
+  });
+  
